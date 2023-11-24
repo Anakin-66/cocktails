84785bbe7f2355770bf917dcf82e1c39e4e593e4
Frangeul Nathan <seekerz17@outlook.fr>
2023-11-24 16:30:01 +0100

    added useEffect

--- src/pages/CocktailRandomPage.jsx
+++ src/pages/CocktailRandomPage.jsx
@@ -1,22 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function CocktailRandomPage () {
 
     const [randomCocktail, setRandomCocktail] = useState(null);
 
-    // Je déclare un boolean pour que la fonction ne s'exécute qu'une fois pour éviter une boucle infinie et faire péter le serveur
-    if (!randomCocktail) {
-        // Je déclare une fonctione asynchrone anonyme car c'est la synthaxe la plus récente pour faire un fetch d'API
-        (async () => {
-            // Je déclare une variable response avec un await car il y a un petit temps d'attente le temps que le fetch récupère les données
-            const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`);  
-            // Grâce à la promesse du dessus je déclare une variable data qui va devoir me retranscrire mon json en js
-            const data = await response.json();   
-            //  J'exécute la fonction de mon useState 
-            setRandomCocktail(data.drinks[0]);
-        // J'exécute la fonction asynchrone avec les doubles parenthèse
-        })();
+        // J'utilise mon hook useEffect pour exécuter du code à un certains moment du chargement du composant
+        useEffect(() => {
+            // Je déclare une fonctione asynchrone anonyme car c'est la synthaxe la plus récente pour faire un fetch d'API
+            (async () => {
+                // Je déclare une variable response avec un await car il y a un petit temps d'attente le temps que le fetch récupère les données
+                const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`);  
+                // Je daclare une variable data avec un await qui va définir un temps d'attente et qui va devoir me retranscrire mon json en js
+                const data = await response.json();   
+                //  J'exécute la fonction de mon useState 
+                setRandomCocktail(data.drinks[0]);
+            // J'exécute la fonction asynchrone avec les doubles parenthèse
+            })();
+            // Le composant sera rendu une seule fois si le tableau est vide à la fin
+        }, [])
+       
         
+   
+
+    const handleButtonClick = () => {
+        (async () => {
+            const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`);  
+            const data = await response.json();   
+            setRandomCocktail(data.drinks[0]);
+        })();
     }
     
 
@@ -26,9 +37,11 @@ function CocktailRandomPage () {
 
             <article>
                 <h2> {randomCocktail.strDrink} </h2>
+                <img src={randomCocktail.strThumb} />
             </article>
         
         }
+        <button onClick={handleButtonClick}> J'ai soif, change de cocktail </button>
         </main>
 
     )
